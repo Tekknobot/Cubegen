cd330d791643f5693b97957bb994c010bc2c1c2c
Tekknobot <thrillatron@hotmail.com>
2022-01-16 06:25:46 -0500

    Minor bug fix for last move when camera is selecting

--- Assets/Scripts/TacticsCamera.cs
+++ Assets/Scripts/TacticsCamera.cs
@@ -29,7 +29,7 @@ public class TacticsCamera : MonoBehaviour
 
         if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(1)) {
             target = playerPrefabs[playerPrefabsIndex++].transform;
-            if (playerPrefabsIndex >=5) {
+            if (playerPrefabsIndex >= 5) {
                 playerPrefabsIndex = 0;
             }
 
@@ -75,7 +75,7 @@ public class TacticsCamera : MonoBehaviour
             playerPrefabs[1].GetComponent<TacticsMove>().FindSelectableTiles();
             playerPrefabs[1].GetComponent<TacticsMove>().turn = true;
             playerPrefabs[1].GetComponent<PlayerMove>().unitTurn = true;
-            playerPrefabs[0].GetComponent<PlayerMove>().tempGO = playerPrefabs[1].transform.gameObject;
+            playerPrefabs[1].GetComponent<PlayerMove>().tempGO = playerPrefabs[1].transform.gameObject;
         }  
 
         if (target == playerPrefabs[2].transform) {    
@@ -84,7 +84,7 @@ public class TacticsCamera : MonoBehaviour
             playerPrefabs[2].GetComponent<TacticsMove>().FindSelectableTiles();
             playerPrefabs[2].GetComponent<TacticsMove>().turn = true;
             playerPrefabs[2].GetComponent<PlayerMove>().unitTurn = true;
-            playerPrefabs[0].GetComponent<PlayerMove>().tempGO = playerPrefabs[2].transform.gameObject;
+            playerPrefabs[2].GetComponent<PlayerMove>().tempGO = playerPrefabs[2].transform.gameObject;
         }                  
 
         if (target == playerPrefabs[3].transform) {    
@@ -93,7 +93,7 @@ public class TacticsCamera : MonoBehaviour
             playerPrefabs[3].GetComponent<TacticsMove>().FindSelectableTiles();
             playerPrefabs[3].GetComponent<TacticsMove>().turn = true;
             playerPrefabs[3].GetComponent<PlayerMove>().unitTurn = true;
-            playerPrefabs[0].GetComponent<PlayerMove>().tempGO = playerPrefabs[3].transform.gameObject;
+            playerPrefabs[3].GetComponent<PlayerMove>().tempGO = playerPrefabs[3].transform.gameObject;
         }  
 
         if (target == playerPrefabs[4].transform) {    
@@ -102,7 +102,7 @@ public class TacticsCamera : MonoBehaviour
             playerPrefabs[4].GetComponent<TacticsMove>().FindSelectableTiles();
             playerPrefabs[4].GetComponent<TacticsMove>().turn = true;
             playerPrefabs[4].GetComponent<PlayerMove>().unitTurn = true;
-            playerPrefabs[0].GetComponent<PlayerMove>().tempGO = playerPrefabs[4].transform.gameObject;
+            playerPrefabs[4].GetComponent<PlayerMove>().tempGO = playerPrefabs[4].transform.gameObject;
         }                        
     }
 }
