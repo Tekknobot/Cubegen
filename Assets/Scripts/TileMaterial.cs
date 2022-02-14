4fa98acb0c580ae6ced0121d1a51ace8b2cfac7a
Tekknobot <thrillatron@hotmail.com>
2022-02-13 23:54:32 -0500

    Add water depth and tile.

--- Assets/Scripts/TileMaterial.cs
+++ Assets/Scripts/TileMaterial.cs
@@ -8,6 +8,7 @@ public class TileMaterial : MonoBehaviour
     
     public Material[] newMats;    
     public Material defaultMat;
+    public GameObject waterTile;
 
     void Start()
     {
@@ -16,30 +17,29 @@ public class TileMaterial : MonoBehaviour
         foreach (GameObject t in tiles)
         {   
             int calc_dropChance = Random.Range (0, 101);
-            if (calc_dropChance >= 88 && calc_dropChance <= 101) {
+            if (calc_dropChance >= 55 && calc_dropChance <= 101) {
                 Material materialToUse = newMats[0];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             } 
-            if (calc_dropChance >= 55 && calc_dropChance <= 88) {
+            if (calc_dropChance >= 33 && calc_dropChance <= 55) {
                 Material materialToUse = newMats[1];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             }
-            if (calc_dropChance >= 33 && calc_dropChance <= 55) {
+            if (calc_dropChance >= 22 && calc_dropChance <= 33) {
                 Material materialToUse = newMats[2];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             }
-            if (calc_dropChance >= 22 && calc_dropChance <= 33) {
+            if (calc_dropChance >= 11 && calc_dropChance <= 22) {
                 Material materialToUse = newMats[3];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             }
-            if (calc_dropChance >= 11 && calc_dropChance <= 22) {
-                Material materialToUse = newMats[4];
-                t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
-            }  
             if (calc_dropChance >= 0 && calc_dropChance <= 11) {
                 Material materialToUse = newMats[4];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
-            }                                                                                               
+                t.transform.position = new Vector3(t.transform.position.x, -0.125f, t.transform.position.z);
+                t.transform.localScale = new Vector3(t.transform.localScale.x, 0.75f, t.transform.localScale.z);
+                Instantiate(waterTile, new Vector3(t.transform.position.x, 0.25f, t.transform.position.z), Quaternion.Euler(90, 0, 0));
+            }                                                                                              
         }
     }
 }
