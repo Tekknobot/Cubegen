58fbb6225c27296fae12210fc22fcbbf90935451
Tekknobot <thrillatron@hotmail.com>
2022-02-14 00:29:41 -0500

    Move to tile on map start.

--- Assets/Scripts/TacticsMove.cs
+++ Assets/Scripts/TacticsMove.cs
@@ -42,7 +42,11 @@ public class TacticsMove : MonoBehaviour
 
         if (this.transform.gameObject.tag == "Player") {
             TurnManager.AddUnit(this);
-        }        
+        } 
+
+        FindSelectableTiles();
+        MoveToTile(GetTargetTile(gameObject));
+        RemoveSelectableTiles();
     }
 
     public void GetCurrentTile()
--- Assets/Scripts/TileMaterial.cs
+++ Assets/Scripts/TileMaterial.cs
@@ -38,7 +38,7 @@ public class TileMaterial : MonoBehaviour
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
                 t.transform.position = new Vector3(t.transform.position.x, -0.125f, t.transform.position.z);
                 t.transform.localScale = new Vector3(t.transform.localScale.x, 0.75f, t.transform.localScale.z);
-                Instantiate(waterTile, new Vector3(t.transform.position.x, 0.25f, t.transform.position.z), Quaternion.Euler(90, 0, 0));
+                Instantiate(waterTile, new Vector3(t.transform.position.x, 0.26f, t.transform.position.z), Quaternion.Euler(90, 0, 0));
             }                                                                                              
         }
     }
