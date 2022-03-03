850b3429ab58df4cef70656b0fabfcdb9b4480f4
Tekknobot <thrillatron@hotmail.com>
2022-03-02 23:59:47 -0500

    Push.

--- Assets/Scripts/NPCMove.cs
+++ Assets/Scripts/NPCMove.cs
@@ -54,7 +54,7 @@ public class NPCMove : TacticsMove
             FindNearestTarget();
             CalculatePath();
             FindSelectableTiles();
-            actualTargetTile.target = true;            
+            //actualTargetTile.target = true;            
             pushed = false;
         }
 
