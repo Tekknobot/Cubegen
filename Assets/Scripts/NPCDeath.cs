5e250de2480f741b9dad4d52867bdb4ef20d6bf7
Tekknobot <thrillatron@hotmail.com>
2022-03-06 04:16:57 -0500

    Random push.

--- Assets/Scripts/NPCDeath.cs
+++ Assets/Scripts/NPCDeath.cs
@@ -21,11 +21,15 @@ public class NPCDeath : MonoBehaviour
     }
 
     IEnumerator DestroyObject() {
-        this.gameObject.GetComponent<Rigidbody>().useGravity = true;
-        this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; 
-        Tile t = this.gameObject.transform.GetComponent<TacticsMove>().GetTargetTile(this.gameObject); 
-        this.gameObject.transform.position = new Vector3(t.transform.position.x, 1, t.transform.position.z);     
+        this.transform.GetComponent<Rigidbody>().useGravity = true;
+        this.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; 
+        Tile t = this.transform.GetComponent<TacticsMove>().GetTargetTile(this.gameObject); 
+        this.transform.position = new Vector3(t.transform.position.x, 1, t.transform.position.z);     
         yield return new WaitForSeconds(1);
+        GameObject[] playerUnits = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject playerUnit in playerUnits) {
+            playerUnit.transform.GetComponent<TacticsMove>().moving = false;
+        }        
         Instantiate(explosion, this.transform.position, Quaternion.Euler(45, -45, 0));
         this.gameObject.GetComponent<NPCMove>().enabled = false;
         this.gameObject.GetComponent<NPCAttack>().enabled = false;
