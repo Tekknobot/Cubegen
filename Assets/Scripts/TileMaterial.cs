0853c1173f43f8516a18d277010a091b7ca95bbf
Tekknobot <thrillatron@hotmail.com>
2022-03-08 20:46:07 -0500

    Make water tile chiles of cube.\

--- Assets/Scripts/TileMaterial.cs
+++ Assets/Scripts/TileMaterial.cs
@@ -42,7 +42,8 @@ public class TileMaterial : MonoBehaviour
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
                 t.transform.position = new Vector3(t.transform.position.x, -0.125f, t.transform.position.z);
                 t.transform.localScale = new Vector3(t.transform.localScale.x, 0.75f, t.transform.localScale.z);
-                Instantiate(waterTile, new Vector3(t.transform.position.x, 0.26f, t.transform.position.z), Quaternion.Euler(90, 0, 0));
+                var waterTileGO = Instantiate(waterTile, new Vector3(t.transform.position.x, 0.26f, t.transform.position.z), Quaternion.Euler(90, 0, 0));
+                waterTileGO.transform.parent = t.transform;
             }  
 
             if (calc_dropChance >= 0 && calc_dropChance <= 22) {
