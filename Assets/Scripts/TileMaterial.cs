c5183f020f0cff00dc949be7eb013c4381d7e250
Tekknobot <thrillatron@hotmail.com>
2022-03-10 01:17:05 -0500

    Adjust spawn tilematerial chance.

--- Assets/Scripts/TileMaterial.cs
+++ Assets/Scripts/TileMaterial.cs
@@ -20,25 +20,25 @@ public class TileMaterial : MonoBehaviour
         foreach (GameObject t in tiles)
         {   
             int calc_dropChance = Random.Range (0, 101);
-            if (calc_dropChance >= 66 && calc_dropChance <= 101) {
+            if (calc_dropChance >= 70 && calc_dropChance <= 101) {
                 Material materialToUse = newMats[0];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             } 
-            if (calc_dropChance >= 44 && calc_dropChance <= 66) {
+            if (calc_dropChance >= 40 && calc_dropChance <= 70) {
                 Material materialToUse = newMats[1];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             }
-            if (calc_dropChance >= 33 && calc_dropChance <= 44) {
+            if (calc_dropChance >= 30 && calc_dropChance <= 40) {
                 Material materialToUse = newMats[2];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             }
-            if (calc_dropChance >= 27 && calc_dropChance <= 33) {
+            if (calc_dropChance >= 27 && calc_dropChance <= 30) {
                 Material materialToUse = newMats[3];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
                 t.transform.position = new Vector3(t.transform.position.x, 0.125f, t.transform.position.z);
                 t.transform.localScale = new Vector3(t.transform.localScale.x, 1.25f, t.transform.localScale.z);               
             }
-            if (calc_dropChance >= 22 && calc_dropChance <= 27) {
+            if (calc_dropChance >= 24 && calc_dropChance <= 27) {
                 Material materialToUse = newMats[4];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
                 t.transform.position = new Vector3(t.transform.position.x, -0.125f, t.transform.position.z);
@@ -46,19 +46,19 @@ public class TileMaterial : MonoBehaviour
                 var waterTileGO = Instantiate(waterTile, new Vector3(t.transform.position.x, 0.26f, t.transform.position.z), Quaternion.Euler(90, 0, 0));
                 waterTileGO.transform.parent = t.transform;
             }  
-            if (calc_dropChance >= 4 && calc_dropChance <= 22) {
+            if (calc_dropChance >= 5 && calc_dropChance <= 24) {
                 Material materialToUse = newMats[4];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
             }   
 
             //buildings
-            if (calc_dropChance >= 3 && calc_dropChance <= 4) {
+            if (calc_dropChance >= 4 && calc_dropChance <= 6) {
                 Material materialToUse = newMats[4];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
                 var buildingTileGO = Instantiate(buildingsTile, new Vector3(t.transform.position.x, 0.875f, t.transform.position.z), Quaternion.Euler(45, -45, 0));
                 buildingTileGO.transform.parent = t.transform;
             } 
-            if (calc_dropChance >= 2 && calc_dropChance <= 3) {
+            if (calc_dropChance >= 2 && calc_dropChance <= 4) {
                 Material materialToUse = newMats[4];
                 t.GetComponent<Renderer>().material.CopyPropertiesFromMaterial(materialToUse);
                 var buildingTileGO1 = Instantiate(buildingsTile1, new Vector3(t.transform.position.x, 0.875f, t.transform.position.z), Quaternion.Euler(45, -45, 0));
